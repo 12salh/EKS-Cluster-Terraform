dcc3240588f767ed99a95482f4d0563292057bdf
Mohamed Saleh <151667007+12salh@users.noreply.github.com>
2026-02-06 00:50:10 +0200

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "nti-terraform-states"
-    key            = "eks/nonprod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "nti-terraform-locks"
-    encrypt        = true
-  }
-}
