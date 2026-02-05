67d2894e4d31222c0e36f6fc8f296d58dd5bafef
Mohamed Saleh <151667007+12salh@users.noreply.github.com>
2026-02-05 23:28:48 +0200

    Add files via upload

---
+++ Ingress/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "nti-terraform-states-1"
+    key            = "eks/nonprod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "nti-terraform-locks-1"
+    encrypt        = true
+  }
+}
