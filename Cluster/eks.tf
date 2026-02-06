3e15eb51b240bfcd4e34e6f63a18c95706250480
Mohamed Saleh <151667007+12salh@users.noreply.github.com>
2026-02-06 05:23:48 +0200

    Update eks.tf

--- Cluster/eks.tf
+++ Cluster/eks.tf
@@ -18,28 +18,6 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 }
 
 
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "${local.name_prefix}-eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action   = "sts:AssumeRole"
-    }]
-  })
-}
-
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-
-
 # resource "aws_eks_cluster" "this" {
 #   name     = local.cluster_name
 #   role_arn = aws_iam_role.eks_cluster_role.arn
