8c3e2e646ed9eb27a6681ec2f26384a68fbbca34
Mohamed Saleh <151667007+12salh@users.noreply.github.com>
2026-02-06 04:51:11 +0200

    Create test.tf

---
+++ Cluster/test.tf
@@ -0,0 +1,32 @@
+resource "aws_eks_access_entry" "azure_pipeline" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = local.azure_devops_role_arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "azure_pipeline_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = local.azure_devops_role_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+# variable "azure_devops_role_arn" {
+#   description = "arn:aws:iam::251251171133:role/AzureAgent"
+#   type        = string
+# }
+
+
+resource "aws_eks_access_entry" "nodes" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_iam_role.node_role.arn
+  type          = "EC2_LINUX"
+}
+
+
+locals {
+  azure_devops_role_arn = "arn:aws:iam::251251171133:role/AzureAgent"
+}
