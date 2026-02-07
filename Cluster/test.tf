6b80f5f1bcc7ed34eb5a848e8d85c752f31bc037
Mohamed Saleh <151667007+12salh@users.noreply.github.com>
2026-02-07 13:32:23 +0200

    Update test.tf

--- Cluster/test.tf
+++ Cluster/test.tf
@@ -6,6 +6,10 @@ resource "aws_eks_access_entry" "azure_pipeline" {
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = local.azure_devops_role_arn
   type          = "STANDARD"
+
+  depends_on = [
+    aws_eks_cluster.this
+  ]
 }
 
 resource "aws_eks_access_policy_association" "azure_pipeline_admin" {
@@ -16,4 +20,19 @@ resource "aws_eks_access_policy_association" "azure_pipeline_admin" {
   access_scope {
     type = "cluster"
   }
+
+  depends_on = [
+    aws_eks_access_entry.azure_pipeline
+  ]
+}
+
+
+resource "aws_eks_access_entry" "nodes" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_iam_role.node_role.arn
+  type          = "EC2_LINUX"
+
+  depends_on = [
+    aws_eks_cluster.this
+  ]
 }
